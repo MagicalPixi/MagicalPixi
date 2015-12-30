a37ddbc3b9e8a7ac96d674044d50303a356a95ec
zhouyg <yunge@taovip.com>
2015-12-30 18:58:28 +0800

    结构

---
+++ public/js/components/SpritePreview.jsx
@@ -0,0 +1,18 @@
+let React = require('react');
+
+class SpritePreview extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div id="spritePreviewBox">
+      </div>
+    )
+  }
+}
+
+module.exports = SpritePreview;
