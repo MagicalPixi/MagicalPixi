d3205f856f21ea7f6b5dfe71ea0766e5fd777af8
zhou-yg <zhou.y.g890@gmail.com>
2016-01-16 01:32:17 +0800

    修改逻辑，存素材时，同时生成zip。

--- api/saveProperties.js
+++ api/saveProperties.js
@@ -3,9 +3,13 @@
  */
 var Sprite = require('../models/Sprite');
 
-module.exports = function (req, res) {
+var buildMaterialZipSource = require('../services/buildMaterialZipSource');
 
-  console.log(req.body);
+var archiverZip = require('../services/archiverDownloadZip');
+
+var MaterialZip = require('../models/MaterialZip');
+
+module.exports = function (req, res) {
 
   var type = req.body.spriteType;
   var name = req.body.spriteName;
@@ -14,18 +18,42 @@ module.exports = function (req, res) {
 
   var userFlag = req.session.userFlag;
 
-  Sprite.save({
+  var materialObj = {
     resourceUrl,
     userFlag,
     type,
     name,
     properties
-  }).then(function(result){
-    console.log(result);
+  };
+
+  console.log('materialObj:',materialObj);
+
+  //存入素材
+  Sprite.save(materialObj).then(function(result){
 
     res.json({result});
 
   }).catch(function (err) {
     res.json({err})
+  });
+
+
+  //@TODO 生成zip的逻辑，待优化
+  console.log('生成sourceObj');
+  //同时，生成一个zip包
+  buildMaterialZipSource(materialObj).then(function (sourceObj) {
+
+    console.log('sourceObj:',sourceObj);
+
+    archiverZip(name+'.zip',sourceObj).then(function (zipPath) {
+
+      //存储zip包和素材名字至数据库
+      MaterialZip.insertOne({
+        zipPath,
+        name
+      }).then(function (result) {
+        console.log(result.result);
+      })
+    });
   });
 };
