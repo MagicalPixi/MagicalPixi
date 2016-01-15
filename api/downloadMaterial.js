3076a052f39067be8bbfdf10150170f07abfb8db
zhou-yg <zhou.y.g890@gmail.com>
2016-01-16 01:34:14 +0800

    下载逻辑简化，仅是读取zip

--- api/downloadMaterial.js
+++ api/downloadMaterial.js
@@ -3,68 +3,30 @@
  */
 var path = require('path');
 
-var Sprite = require('../models/Sprite');
-
-var spriteScriptsTemplate = require('../services/spriteScriptsTemplate');
-
-var archiverZip = require('../services/archiverDownloadZip');
-
-var resourceObjBuild = function (resourceUrl) {
-  var resourceArr = [resourceUrl];
-
-  if(resourceUrl.indexOf('.json') !== -1){
-    var p = path.parse(resourceUrl);
-    var name = p.name;
-    var dir = p.name;
-    resourceArr.push(
-      path.resolve(dir,name + '.png')
-    );
-  }
-
-  return resourceArr.map(function (url) {
-    return [
-      path.parse(url).base,
-      path.join(__dirname,'../public',url)
-    ]
-  }).reduce(function (init, next) {
-    init[next[0]] = next[1];
-    return init;
-  },{})
-};
+var MaterialZip = require('../models/MaterialZip');
 
 //GET0
-module.exports = function (req, res) {
+module.exports = function (req, res,next) {
   var name = req.query.name;
 
-  console.log('name',name);
-
-  Sprite.findOne({name}).then(function (result) {
+  MaterialZip.findOne({
+    name
+  }).then(function (result) {
 
     if(result){
-      var properties = JSON.parse(result.properties);
-
-      var scripts = spriteScriptsTemplate(
-        result.type,
-        properties
+      res.download(
+        result.zipPath,
+        name+'.zip'
       );
-
-      var resourceObj = resourceObjBuild(result.resourceUrl);
-
-      resourceObj = Object.assign(resourceObj,{
-        [name+'.js']:new Buffer(scripts),
-      });
-
-      archiverZip('d1.zip',resourceObj)
-        .then(function (zipPath) {
-          console.log('zipPath:',zipPath);
-
-          res.download(
-            zipPath,
-            name+'.zip'
-          );
-      }).catch(function (err) {
-        console.log('archiver error:',err);
+    }else{
+      res.json({
+        error:`${name} not found`
       });
     }
+  }).catch(function (err) {
+
+    console.log('materialZip findOne error:',err);
+
+    next(err);
   });
 };
