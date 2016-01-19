ece928a8e47ada92a85056f47ca40f2ad175ea17
zhou-yg <zhou.y.g890@gmail.com>
2016-01-19 23:21:06 +0800

    add favicon.ico

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var session = require('express-session');
 var path = require('path');
 //var favicon = require('serve-favicon');
+var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var multiParty = require('connect-multiparty');
-
+var favicon = require('serve-favicon');
 var routes = require('./routes/index');
 
 var app = express();
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.resolve(__dirname,'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
