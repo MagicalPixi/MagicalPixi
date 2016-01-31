8038a522746751f44379bd621998af84ae784a07
zhou-yg <zhou.y.g890@gmail.com>
2016-01-31 21:56:56 +0800

    daynamic download zip

---
+++ api/buildDownloadZip.js
@@ -0,0 +1,50 @@
+/**
+ * Created by zyg on 16/1/31.
+ */
+var ObjectId = require('mongodb').ObjectID;
+
+var Sprite = require('../models/Sprite');
+
+var buildMaterialZipSource = require('../services/buildMaterialZipSource');
+
+var archiverZip = require('../services/archiverDownloadZip');
+
+var MaterialZip = require('../models/MaterialZip');
+
+
+module.exports = function (req, res,next) {
+
+  var id = req.query.id;
+  var name = req.query.name;
+
+  var condition = null;
+
+  if(id){
+    condition = {
+      _id:ObjectId(id)
+    }
+  }else if(name){
+    condition = { name }
+  }
+
+  console.log('condition:',condition);
+
+  if(!condition){
+    return next(new Error('no id or name'))
+  }
+
+  Sprite.findOne(condition).then(function (result) {
+
+    buildMaterialZipSource(result).then(function (sourceObj) {
+      archiverZip(result.name+'.zip',sourceObj).then(function (zipPath) {
+
+        res.download(
+          zipPath,
+          result.name+'.zip'
+        )
+      })
+    })
+  })
+
+
+};
--- api/downloadMaterial.js
+++ api/downloadMaterial.js
@@ -2,7 +2,6 @@
  * Created by zyg on 16/1/13.
  */
 var path = require('path');
-var ObjectId = require('mongodb').ObjectID;
 
 var MaterialZip = require('../models/MaterialZip');
 
