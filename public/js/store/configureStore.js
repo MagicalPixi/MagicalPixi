42dcd6feac60f427705695bcc3ac69c87b31a925
zhou-yg <zhou.y.g890@gmail.com>
2016-02-05 14:42:34 +0800

    clear

--- public/js/store/configureStore.js
+++ public/js/store/configureStore.js
@@ -4,13 +4,13 @@ import rootReducer from '../reducers'
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState)
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers/index')
-      store.replaceReducer(nextReducer)
-    })
-  }
+  //if (module.hot) {
+  //  // Enable Webpack hot module replacement for reducers
+  //  module.hot.accept('../reducers', () => {
+  //    const nextReducer = require('../reducers/index')
+  //    store.replaceReducer(nextReducer)
+  //  })
+  //}
 
   return store
 }
