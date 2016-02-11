8b9ec712f94fde3f00a63e0aca2961e444b2898f
zhou-yg <zhou.y.g890@gmail.com>
2016-02-11 23:07:42 +0800

    EditText Component Start

---
+++ public/componentBasic/EditText/EditText.jsx
@@ -0,0 +1,24 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+
+const T = React.PropTypes;
+
+class EditText extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+
+    let {style} = this.props;
+
+    return (
+      <p style={style} >
+
+      </p>
+    )
+  }
+}
+
+module.exports = EditText;
