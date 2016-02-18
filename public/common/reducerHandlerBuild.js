0ba54c9c9aef36c50fccae47ae074dd5f10a21f9
zhou-yg <zhou.y.g890@gmail.com>
2016-02-18 23:40:55 +0800

    reducer handler生成。

---
+++ public/common/reducerHandlerBuild.js
@@ -0,0 +1,15 @@
+module.exports = function (handler,defaultState) {
+
+  return function handlerFn(state,action){
+
+    if(state === undefined){
+      state = defaultState;
+    }
+
+    if(handler[action.type]){
+      return handler[action.type](state,action)
+    }
+
+    return state;
+  }
+}
