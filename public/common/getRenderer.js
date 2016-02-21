b3ce6ecbd9c3bc49e66957cf6d8006133f7f07f4
zhou-yg <zhou.y.g890@gmail.com>
2016-02-22 00:00:15 +0800

    clear

--- public/common/getRenderer.js
+++ public/common/getRenderer.js
@@ -1,34 +1,7 @@
-/**
- * Created by zyg on 16/1/4.
- */
-var r = require('./isSupportWebGL')();
-var renderer = null;
-if(r){
-  renderer= new PIXI.WebGLRenderer(640, 1004, {
-      transparent:true
-    }
-  );
-}else{
-  renderer= new PIXI.CanvasRenderer(640, 1004, {
-      transparent:true
-    }
-  );
-}
 
 module.exports = function () {
-  var renderer = null;
-
-  if(r){
-    renderer= new PIXI.WebGLRenderer(320, 502, {
-        transparent:true
-      }
-    );
-  }else{
-    renderer= new PIXI.CanvasRenderer(320, 502, {
-        transparent:true
-      }
-    );
-  }
-
-  return renderer;
+  return new PIXI.autoDetectRenderer(640,1004, {
+      transparent:true
+    }
+  );
 };
