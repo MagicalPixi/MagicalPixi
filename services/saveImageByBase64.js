a5050a7bf69fd683a4265d262c3f7f0c25782837
zhou-yg <zhou.y.g890@gmail.com>
2016-03-08 23:29:11 +0800

    保存资源son和png至 public/basic文件夹

---
+++ services/saveImageByBase64.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+
+var dir = path.resolve(__dirname,'../public/basic/');
+
+var countName = 0;
+
+var base64Prefix = /^data:image\/(png|gif|jpeg);base64,/;
+
+
+var savePngName = function (name,suffix) {
+
+  if(!name){
+    name = '';
+  }
+  if(!suffix){
+    suffix = 'png';
+  }
+
+
+  var timeStamp = Date.now();
+
+  return `${name}_${timeStamp}_${++countName}.${suffix}`;
+};
+
+module.exports = function (base64,option) {
+
+  if(!base64){
+    base64 = '';
+  }
+  if(!option){
+    option = {};
+  }
+
+  var base64Clean = base64.replace(base64Prefix,'');
+
+  var base64Buffer = new Buffer(base64Clean,'base64');
+
+  var filename = savePngName();
+
+  var fullFilename = path.join(dir,filename);
+
+  return new Promise(function (resolve) {
+
+    fs.writeFile(fullFilename,base64Buffer, function (err) {
+      console.log(err);
+      if(err){
+        throw err
+      }
+      resolve(filename);
+    });
+  });
+
+};
---
+++ services/savePixiJson.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var _ = require('lodash');
+
+var path = require('path');
+var fs = require('fs');
+
+var dir = path.resolve(__dirname,'../public/basic/');
+
+var imageExt = /\.(png|jpg|gif)/;
+
+module.exports = function (pixiJson) {
+
+  if(_.isString(pixiJson)){
+    pixiJson = JSON.parse(pixiJson);
+  }
+
+  var imageName = pixiJson.meta.image;
+
+  var jsonName = imageName.replace(imageExt,'') + '.json';
+
+  var fullFilename = path.join(dir,jsonName);
+
+  pixiJson = JSON.stringify(pixiJson);
+
+  return new Promise(function (resolve) {
+
+    fs.writeFile(fullFilename,pixiJson, function (err) {
+      console.log(err);
+      if(err){
+        throw err
+      }
+      resolve(jsonName);
+    });
+  })
+};
