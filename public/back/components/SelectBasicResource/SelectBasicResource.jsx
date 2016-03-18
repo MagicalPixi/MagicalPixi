fbbde4addc66f97be0ccd650a8cebf753a856c4d
zhou-yg <zhou.y.g890@gmail.com>
2016-03-19 01:22:36 +0800

    basic选择列表

--- public/back/components/SelectBasicResource/SelectBasicResource.jsx
+++ public/back/components/SelectBasicResource/SelectBasicResource.jsx
@@ -17,12 +17,38 @@ class SelectBasicResource extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.select = this.select.bind(this);
+  }
+
+  select(e){
+    var {resources,onSelect} = this.props;
+    var index = parseInt(e.currentTarget.getAttribute('data-index'));
+
+    log(resources,index);
+
+    onSelect(resources[index]);
   }
 
   render() {
+
+    var {resources} = this.props;
+
     return (
       <div id="selectBasicResource">
+        <ul className="resources">
+        {resources.map((basic,i)=>{
+          var {name,resourceName} = basic;
+          var key = `basic${i}`;
 
+          return (
+            <li key={key} data-index={i} onClick={this.select}>
+              <img src={`${resourceName}.png`} />
+              <p className="name">{name}</p>
+            </li>
+          )
+        })}
+        </ul>
       </div>
     )
   }
