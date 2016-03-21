398b0b279544001fb0c8a2133a4aa8acfa8116c9
zhou-yg <zhou.y.g890@gmail.com>
2016-03-21 23:20:40 +0800

    pre

---
+++ public/back/components/PlayerPacker/PlayerPacker.jsx
@@ -0,0 +1,24 @@
+require('./PlayerPacker.scss'); 
+ import React,{Component} from 'react' 
+ import ReactDOM from 'react-dom' 
+ const T = React.PropTypes; 
+ var propTypes = { 
+ }; 
+ var defaultProps = { 
+ }; 
+ class PlayerPacker extends Component { 
+ constructor(props){ 
+ super(props); 
+ this.state = {
+ }; 
+ } 
+ render(){ 
+ return ( 
+ <div id="playerPacker"> 
+ </div> 
+ ) 
+ } 
+ } 
+ PlayerPacker.propTypes = propTypes; 
+ PlayerPacker.defaultProps = defaultProps; 
+ module.exports = PlayerPacker;
