ee3010ec638c5e4f1b983994dd208b16ce950123
zhou-yg <zhou.y.g890@gmail.com>
2016-03-27 01:08:07 +0800

    接入保存和编辑

--- public/back/components/PlayerList/PlayerList.jsx
+++ public/back/components/PlayerList/PlayerList.jsx
@@ -4,8 +4,9 @@ import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
 const T = React.PropTypes;
 
-import ItemList from '../../componentsLayout/ItemList'
+import autoBind from 'react-autobind'
 
+import ItemList from '../../componentsLayout/ItemList'
 import Popup from '../Popup'
 import PlayerPacker from '../PlayerPacker'
 
@@ -13,6 +14,7 @@ var propTypes = {};
 
 var defaultProps = {
   data:[],
+  basics:[],
 };
 
 class PlayerList extends Component {
@@ -20,29 +22,57 @@ class PlayerList extends Component {
     super(props);
     this.state = {};
 
-    this.newOne = this.newOne.bind(this);
+
+    autoBind(this);
   }
 
   componentDidMount(){
     var {basics} = this.props;
 
-    this.close = Popup(<PlayerPacker basics={basics}></PlayerPacker>)
+    //this.close = Popup(<PlayerPacker onPacker={this.packerCompleted} basics={basics}></PlayerPacker>)
   }
   componentDidUpdate(){
-    var {basics} = this.props;
+    //var {basics} = this.props;
+    //
+    //if(this.close){
+    //  this.close();
+    //}
+    //this.close = Popup(<PlayerPacker onPacker={this.packerCompleted} basics={basics}></PlayerPacker>)
+  }
 
-    if(this.close){
-      this.close();
-    }
-    this.close = Popup(<PlayerPacker basics={basics}></PlayerPacker>)
+  packerCompleted(player){
+    log(player);
+
+    this.props.actions.playerAdd(player);
+  }
+
+  packerEditCompleted(player){
+
+    this.props.actions.playerEdit(player);
   }
 
   newOne(){
-
+    var {basics} = this.props;
+    this.close = Popup(
+      <PlayerPacker
+        onPacker={this.packerCompleted}
+        basics={basics} />
+    )
   }
 
-  edit(){
+  edit(e){
+    var selectIndex = parseInt(e.target.getAttribute('data-i'));
+    var {basics,data} = this.props;
 
+    var selectPlayer = data[selectIndex];
+
+    this.close = Popup(
+      <PlayerPacker
+        player={selectPlayer}
+        onPacker={this.packerEditCompleted}
+        basics={basics}
+        />
+    )
   }
 
   render() {
@@ -53,7 +83,7 @@ class PlayerList extends Component {
       <div id="playerList">
 
         <header className="top-container">
-          <h3 className="title" >高级精灵列表</h3>
+          <h3 className="title" >合成素材列表</h3>
           <button onClick={this.newOne} className="weui_btn weui_btn_mini weui_btn_primary add-new">+&nbsp;新建高级精灵</button>
         </header>
 
@@ -70,7 +100,9 @@ class PlayerList extends Component {
                   删除
                 </span>
 
-                <a className="operation" onClick={this.edit.bind(this)} href="javascript:void 0">编辑</a>
+                <a className="operation" data-i={i} onClick={this.edit} href="javascript:void 0">
+                  编辑
+                </a>
 
                 <span className="operation" href="javascript:void 0">
                   下载
