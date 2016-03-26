0bf659dff9c2d9a4dd38a961560075079664f472
zhou-yg <zhou.y.g890@gmail.com>
2016-03-27 00:59:40 +0800

    终于save了

--- api/playerSave.js
+++ api/playerSave.js
@@ -3,6 +3,26 @@
 var Player = require('../models/Player');
 
 module.exports = function (req, res) {
+  var id = req.body.id;
+  var name = req.body.name;
+  var childSprites = req.body.childSprites;
 
+  console.log(childSprites);
 
+  Player.save({
+    id,
+    name,
+    childSprites
+  }).then(function (result) {
+
+    res.json({
+      result:result.ops[0]._id
+    });
+  }).catch(function (err) {
+    console.log('err:',err);
+
+    res.json({
+      err
+    })
+  });
 };
