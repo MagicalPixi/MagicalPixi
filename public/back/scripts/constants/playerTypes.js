a8ff51be64fd5aca562545e36542d11add7e5e8d
zhou-yg <zhou.y.g890@gmail.com>
2016-03-27 01:06:39 +0800

    more type

--- public/back/scripts/constants/playerTypes.js
+++ public/back/scripts/constants/playerTypes.js
@@ -1,2 +1,6 @@
 //export const X_X = ''
 export const INIT_PLAYER_LIST = 'INIT_PLAYER_LIST';
+
+export const PLAYER_ADD = 'PLAYER_ADD';
+
+export const PLAYER_EDIT = 'PLAYER_EDIT';
--- public/back/scripts/reducers/players.js
+++ public/back/scripts/reducers/players.js
@@ -1,10 +1,21 @@
-import {INIT_PLAYER_LIST} from '../constants/playerTypes'
+import {INIT_PLAYER_LIST,PLAYER_ADD,PLAYER_EDIT} from '../constants/playerTypes'
 
 import reducerHandlerBuild from '../../../common/reducerHandlerBuild'
+
 const handler = {
   [INIT_PLAYER_LIST](state, action) {
 
-    return state;
+    return action.players;
+  },
+  [PLAYER_ADD](state,action){
+
+    return state.concat(action.player);
+  },
+  [PLAYER_EDIT](state,action){
+
+    return state.map((player)=>{
+      return player.id === action.player.id ? action.player : player
+    });
   }
 };
 
