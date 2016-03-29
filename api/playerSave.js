01abeb3aba6c6d743f384992322087064c37b2fb
zhou-yg <zhou.y.g890@gmail.com>
2016-03-29 23:20:37 +0800

    生成合成素材的对应png和json描述

--- api/playerSave.js
+++ api/playerSave.js
@@ -1,22 +1,77 @@
 'use strict'
 
 var Player = require('../models/Player');
+var saveImageByBase64 = require('../services/saveImageByBase64');
+
+var savePixiJson = require('../services/savePixiJson');
 
 module.exports = function (req, res) {
   var id = req.body.id;
+  var type = req.body.type;
   var name = req.body.name;
+  var pngBase64 = req.body.base64;
+  var json = req.body.pixiJson;
   var childSprites = req.body.childSprites;
 
   console.log(childSprites);
+  json = JSON.parse(json);
 
-  Player.save({
-    id,
-    name,
-    childSprites
+  saveImageByBase64(pngBase64).then(function (pngResult) {
+
+    var filename = pngResult.filename;
+    var pngUrl = pngResult.resourcePngUrl;
+
+    json.meta.image = filename;
+
+    return Promise.race([
+      new Promise(function (resolve) {
+        if(type === 'mc'){
+          savePixiJson(json).then(function (jsonResult) {
+
+            var jsonFilename = jsonResult.jsonName;
+            var jsonUrl = jsonResult.resourceJsonUrl;
+
+            resolve({
+              filename:jsonFilename,
+              resourceUrl:jsonUrl
+            });
+          });
+        }
+      }),
+      new Promise(function(resolve){
+        if(type === 'im'){
+          resolve({
+            filename,
+            resourceUrl:pngUrl
+          });
+        }
+      })
+    ]);
+  }).then(function (result) {
+    console.log('save pre:',result);
+
+    var resourceUrl = result.resourceUrl;
+    var resourceName = resourceUrl.replace(/\.(json|png)/, '');
+
+    return Player.save({
+      id,
+      name,
+      type,
+      resourceName,
+      resourceUrl,
+      childSprites
+    })
   }).then(function (result) {
 
+    var savedObj = result.ops[0];
+
     res.json({
-      result:result.ops[0]._id
+      result:{
+        _id:savedObj._id,
+        name,
+        resourceName:savedObj.resourceName,
+        resourceUrl:savedObj.resourceUrl,
+      }
     });
   }).catch(function (err) {
     console.log('err:',err);
--- public/back/scripts/actions/player.js
+++ public/back/scripts/actions/player.js
@@ -3,6 +3,50 @@ import {INIT_PLAYER_LIST,PLAYER_ADD,PLAYER_EDIT} from '../constants/playerTypes'
 import API from '../../../libs/API'
 import ajax from '../../../libs/ajax'
 
+import wrapperImages from '../../../common/wrapperImages'
+import getBase64FromImages from '../../../common/getBase64FromImages'
+import getPixiJsonFromImages from '../../../common/getPixiJsonFromImages'
+
+
+function loadSpritesImg(childSprites){
+  return Promise.all(childSprites.map(spriteOne=>{
+    return spriteOne.basic.originImgUrls
+  }).reduce((pre,next)=>pre.concat(next),[]).map(url=>{
+    var imgObj = new Image();
+    imgObj.src = url;
+    return imgObj;
+  }).map(imgObj=>{
+    return new Promise(resolve=>{
+      imgObj.onload = ()=>{
+        resolve(imgObj);
+      }
+    });
+  }))
+}
+
+function build(imgObjs){
+  var type = ''; //mc精灵,im图片
+
+  var afterWrapperImages = wrapperImages(imgObjs);
+
+  var base64 = getBase64FromImages(afterWrapperImages);
+
+  var pixiJson = getPixiJsonFromImages(afterWrapperImages);
+
+  if(imgObjs.length > 1){
+    type = 'mc';
+  }else{
+    type = 'im';
+  }
+  return new Promise(resolve=>{
+    resolve({
+      type,
+      base64,
+      pixiJson
+    })
+  });
+}
+
 export function initPlayerList() {
   return (dispatch)=> {
     ajax(API.playerFind).get().then((returnData)=>{
@@ -13,6 +57,9 @@ export function initPlayerList() {
           return {
             id:dataOne._id,
             name:dataOne.name,
+            type:dataOne.type,
+            resourceName:dataOne.resourceName,
+            resourceUrl:dataOne.resourceUrl,
             childSprites:JSON.parse(dataOne.childSprites)
           }
         });
@@ -36,7 +83,6 @@ export function initPlayerList() {
  */
 export function playerAdd(player){
 
-
   if(player.name &&
     player.childSprites &&
     !player.childSprites.every(({basic,properties})=>basic&&properties)){
@@ -45,41 +91,52 @@ export function playerAdd(player){
 
   return (dispatch,getState)=>{
 
-    ajax(API.playerSave).post({
-      name:player.name,
-      childSprites:JSON.stringify(player.childSprites),
-    }).then(returnData=>{
+    loadSpritesImg(player.childSprites)
+      .then(build)
+      .then(buildResult=>{
 
-      if(returnData.result){
+        ajax(API.playerSave).post(Object.assign({
+          type,
+          name:player.name,
+          childSprites:JSON.stringify(player.childSprites),
+        },buildResult)).then(returnData=>{
 
-        dispatch({
-          type:PLAYER_ADD,
-          player:Object.assign({
-            id:returnData.result
-          },player)
+          if(returnData.result){
+
+            dispatch({
+              type:PLAYER_ADD,
+              player:Object.assign({},player,returnData.result,{
+                id:returnData.result._id,
+              })
+            });
+          }
         });
-      }
-    });
+      });
   }
 }
 
-export function playerEdit(player){
+export function playerUpdate(player){
 
   return (dispatch,getState)=>{
 
-    ajax(API.playerSave).post({
-      id:player.id,
-      name:player.name,
-      childSprites:JSON.stringify(player.childSprites),
-    }).then(returnData=>{
+    loadSpritesImg(player.childSprites)
+      .then(build)
+      .then(buildResult=>{
 
-      if(returnData.result){
+        ajax(API.playerSave).post(Object.assign({
+          id:player.id,
+          name:player.name,
+          childSprites:JSON.stringify(player.childSprites),
+        },buildResult)).then(returnData=>{
 
-        dispatch({
-          type:PLAYER_EDIT,
-          player
+          if(returnData.result){
+
+            dispatch({
+              type:PLAYER_EDIT,
+              player:Object.assign({},player,returnData.result)
+            });
+          }
         });
-      }
-    });
+      });
   }
 }
