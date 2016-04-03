54259b04ad4be6ff19c9a33713ba79e59a466936
zhou-yg <zhou.y.g890@gmail.com>
2016-04-03 21:46:20 +0800

    修正生产环境下的pack

--- webpack.config.product.js
+++ webpack.config.product.js
@@ -9,10 +9,10 @@ base.output.publicPath = '';
 
 base.entry = {
   index: [
-    path.resolve(__dirname, './public/js/main.jsx')
+    path.resolve(__dirname, './public/back/scripts/main.jsx')
   ],
   edit: [
-    path.resolve(__dirname, './public/js/edit.jsx'),
+    path.resolve(__dirname, './public/back/scripts/edit.jsx'),
   ]
 },
 
