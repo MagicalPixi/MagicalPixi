5a7b0c4049bf4ad1e2397d4a9643b21b4209bdae
zhou-yg <zhou.y.g890@gmail.com>
2016-07-11 23:54:00 +0800

    fix next队列和method前缀的冲突。

--- routes/controller/api.js
+++ routes/controller/api.js
@@ -3,44 +3,51 @@
  */
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash')
 
 var apiDir = '../../api/';
 
-var apiList = fs.readdirSync(path.resolve(__dirname,apiDir));
+var apiList = fs.readdirSync(path.resolve(__dirname, apiDir));
 
 var outputs = apiList.filter(function (filename) {
   return !/^\./.test(filename);
-}).map(apiOne=>{
+}).map(apiOne=> {
 
-  var apiPath = path.resolve(__dirname,apiDir,apiOne);
+  var apiPath = path.resolve(__dirname, apiDir, apiOne);
 
-  if(fs.lstatSync(apiPath).isDirectory()){
+  if (fs.lstatSync(apiPath).isDirectory()) {
     return fs.readdirSync(apiPath).map(function (dirApi) {
-      return  [dirApi,require(path.join(apiPath,dirApi))];
+      return [dirApi, require(path.join(apiPath, dirApi))];
     }).map(function (fn) {
-      fn[1].apiName = fn[0].replace('.js','');
+      fn[1].apiName = fn[0].replace('.js', '');
       fn[1].method = apiOne;
       return fn[1];
     });
-  }else{
+  } else {
     var apiFn = require(apiPath);
-    apiFn.apiName = apiOne.replace('.js','');
+    apiFn.apiName = apiOne.replace('.js', '');
 
-    return require(apiPath);
+    return apiFn;
   }
 
-}).reduce((pre,next)=>{
-  return pre.concat(next);
+}).reduce((init,next)=>{
+
+  if(next.apiName){
+    return init.concat([next])
+  }else{
+    return init.concat(next)
+  }
+
 },[]).map(function (fn) {
 
-  return [
-    fn.apiName,
-    fn
-  ]
-}).reduce(function (initial,next) {
-  initial[next[0]] = next[1];
+    return [
+      fn.apiName,
+      fn
+    ]
+  }).reduce(function (initial, next) {
+    initial[next[0]] = next[1];
 
-  return initial
-},{});
+    return initial
+  }, {});
 
 module.exports = outputs;
